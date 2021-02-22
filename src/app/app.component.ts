c853a5abb0fb372a84d10ff7fbbfb4834132c2c3
Mathessprod <mathesmsm@gmail.com>
2021-02-22 11:34:45 +0000

    analyctics added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import fbcfg from 'src/app/conf/fbcfg';
+
+// Firebase App (the core Firebase SDK) is always required and
+// must be listed before other Firebase SDKs
+import firebase from "firebase/app";
+
+// Add the Firebase services that you want to use
+import "firebase/auth";
+import  "firebase/analytics";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'mathessprod';
+  
+  ngOnInit(): void {
+    firebase.initializeApp(fbcfg);
+    firebase.analytics().logEvent('eventName');
+  }
 }
+
---
+++ src/app/conf/fbcfg.ts
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyB90VYAM8-LCUDuZd9KAOfI8Pp2wh_NYsE",
+    authDomain: "mathessprod.firebaseapp.com",
+    projectId: "mathessprod",
+    storageBucket: "mathessprod.appspot.com",
+    messagingSenderId: "353243690816",
+    appId: "1:353243690816:web:7eebe2ed408b07b37a2ca5",
+    measurementId: "G-0LJ5LWYYY8"
+  };
+  export default firebaseConfig;
